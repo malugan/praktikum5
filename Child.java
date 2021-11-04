d897dbff54ea69fb7b2bd55db1eb2610ca6d2c7e
Frans Junior Wangsa <90809119+malugan@users.noreply.github.com>
2021-11-04 10:21:48 +0800

    Add files via upload

---
+++ Child.java
@@ -0,0 +1,8 @@
+class Parent {
+}
+
+public class Child extends Parent {
+int x;
+public Child() {
+super();
+x = 5; }}
---
+++ percobaan2.java
@@ -0,0 +1,6 @@
+
+public class percobaan2 {
+public static void main(String[] args) {
+Student Wahyu = new Student();
+}
+}
